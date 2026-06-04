212c313a4262d59b2497399edff1c592eb26e36e
kmpachauri <kmpachauri04@gmail.com>
2026-06-04 08:57:42 +0530

    update webhook get response

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -10,6 +10,9 @@ const router = Router();
 router.post('/upi/initiate-registration', initiateRegistrationPayment);
 router.get('/upi/status', getRegistrationPaymentStatus);
 router.post('/upi/callback', handleUpiCallback);
-router.get('/upi/callback', handleUpiCallback);
+router.get('/upi/callback', (req, res, next) => {
+  if (!req.query.client_txn_id) return res.status(200).send('OK');
+  return handleUpiCallback(req, res, next);
+});
 
 module.exports = router;
